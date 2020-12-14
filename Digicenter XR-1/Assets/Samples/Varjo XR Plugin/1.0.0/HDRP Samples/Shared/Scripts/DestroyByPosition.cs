d8849b26b3fa9c9e0b3a5acb4c1b414e14f824ec
lebbs <35425109+lebbs@users.noreply.github.com>
2020-12-14 15:33:09 +0200

    backup

--- Digicenter XR-1/Assets/Samples/Varjo XR Plugin/1.0.0/HDRP Samples/Shared/Scripts/DestroyByPosition.cs
+++ Digicenter XR-1/Assets/Samples/Varjo XR Plugin/1.0.0/HDRP Samples/Shared/Scripts/DestroyByPosition.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DestroyByPosition : MonoBehaviour
 {
-    public float destroyPositionZ = -10;
+    public float destroyPositionY = -1;
+    public float destroyPositionZ = -1;
     void Update()
     {
-        if(transform.position.z < destroyPositionZ) 
+        if(transform.position.y < destroyPositionY ||
+            transform.position.z < destroyPositionZ) 
         {
             Destroy(this.gameObject); //Destroy gameobject if fallen 10 meters below ground level
         }
