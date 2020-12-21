f535c7c7b82e2a1826436178f7fec0bc8db62721
lebbs <35425109+lebbs@users.noreply.github.com>
2020-12-21 14:48:04 +0200

    BackUp

--- Digicenter XR-1/Assets/StartGame.cs
+++ Digicenter XR-1/Assets/StartGame.cs
@@ -2,36 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class StartGame : MonoBehaviour
 {
-    public int score;
-    public GameObject scoreText;
-
+    public GameObject spawner;
     public int health = 100;
-    public GameObject fracturedObject; 
-    
+    public GameObject fracturedObject;
+
     public void OnDamage(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
-            AddScore();
+            GameStart();
             Destroy();
         }
     }
-    public void AddScore()
-    {
-        score += 1;
-        scoreText.GetComponent<TextMesh>().text = "Score: " + score;
-        Debug.Log(score);
-    }
 
+    private void GameStart()
+    {
+        spawner.SetActive(true);
+    }
     public void Destroy()
     {
         Instantiate(fracturedObject, transform.position, transform.rotation);
         Destroy(gameObject);
     }
-
-   
 
 }
