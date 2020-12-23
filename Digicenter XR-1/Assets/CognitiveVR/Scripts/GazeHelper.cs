0998d781b3b9079cb2bf445f827392d3c37354b7
lebbs <35425109+lebbs@users.noreply.github.com>
2020-12-23 14:47:34 +0200

    backup

---
+++ Digicenter XR-1/Assets/CognitiveVR/Scripts/GazeHelper.cs
@@ -0,0 +1,216 @@
+﻿using UnityEngine;
+using System.Collections;
+using CognitiveVR;
+using System;
+#if CVR_AH
+using AdhawkApi;
+using AdhawkApi.Numerics.Filters;
+#endif
+
+//utility code to get player's eye ray
+
+namespace CognitiveVR
+{
+    public static class GazeHelper
+    {
+        public static Ray GetCurrentWorldGazeRay()
+        {
+            return new Ray(GameplayReferences.HMD.position, GetLookDirection());
+        }
+
+#if CVR_PUPIL
+        
+        static Vector3 GetLookDirection()
+        {
+            InitCheck();
+            return gazeDirection;
+        }
+
+        static PupilLabs.GazeController gazeController;
+        static Vector3 gazeDirection = Vector3.forward;
+
+        static void InitCheck()
+        {
+            if (gazeController != null){return;}
+
+            gazeController = UnityEngine.Object.FindObjectOfType<PupilLabs.GazeController>();
+            if (gazeController != null)
+                gazeController.OnReceive3dGaze += ReceiveEyeData;
+            else
+                Debug.LogError("Pupil Labs GazeController is null!");
+            gazeController.OnReceive3dGaze += ReceiveEyeData;
+        }
+
+        static void ReceiveEyeData(PupilLabs.GazeData data)
+        {
+            if (data.Confidence < 0.6f) { return; }
+            gazeDirection = data.GazeDirection;
+        }
+
+#elif CVR_FOVE
+    static Vector3 GetLookDirection()
+    {
+        Fove.Unity.FoveInterface fi = GameplayReferences.FoveInstance;
+        if (fi == null)
+        {
+            return GameplayReferences.HMD.forward;
+        }
+        var eyeRays = fi.GetGazeRays();
+        Vector3 v = new Vector3(eyeRays.left.direction.x, eyeRays.left.direction.y, eyeRays.left.direction.z);
+        return v.normalized;
+    }
+#elif CVR_TOBIIVR
+    static Vector3 lastDirection = Vector3.forward;
+
+    static Vector3 GetLookDirection()
+    {
+        var provider = Tobii.XR.TobiiXR.Internal.Provider;
+
+        if (provider == null)
+        {
+            return GameplayReferences.HMD.forward;
+        }
+        if (provider.EyeTrackingDataLocal.GazeRay.IsValid)
+        {
+            lastDirection = GameplayReferences.HMD.TransformDirection(provider.EyeTrackingDataLocal.GazeRay.Direction);
+        }
+
+        return lastDirection;
+    }
+#elif CVR_NEURABLE
+
+    static Vector3 GetLookDirection()
+    {
+        return Neurable.Core.NeurableUser.Instance.NeurableCam.GazeRay().direction;
+    }
+#elif CVR_AH
+
+    static Vector3 GetLookDirection()
+    {
+        return Calibrator.Instance.GetGazeVector(filterType: FilterType.ExponentialMovingAverage);
+    }
+#elif CVR_SNAPDRAGON
+        static Vector3 GetLookDirection()
+        {
+            return SvrManager.Instance.EyeDirection;
+        }
+#elif CVR_VIVEPROEYE
+
+        static ViveSR.anipal.Eye.SRanipal_Eye_Framework framework;
+        static ViveSR.anipal.Eye.SRanipal_Eye_Framework.SupportedEyeVersion version;
+        static void InitCheck()
+        {
+            if (framework != null) { return; }
+            framework = ViveSR.anipal.Eye.SRanipal_Eye_Framework.Instance;
+            if (framework != null)
+            {
+                version = framework.EnableEyeVersion;
+            }
+        }
+
+        static Vector3 lastDir = Vector3.forward;
+        static Vector3 GetLookDirection()
+        {
+            InitCheck();
+            var ray = new Ray();
+            if (version == ViveSR.anipal.Eye.SRanipal_Eye_Framework.SupportedEyeVersion.version1)
+            {
+                if (ViveSR.anipal.Eye.SRanipal_Eye.GetGazeRay(ViveSR.anipal.Eye.GazeIndex.COMBINE, out ray))
+                {
+                    lastDir = GameplayReferences.HMD.TransformDirection(ray.direction);
+                }
+            }
+            else
+            {
+                if (ViveSR.anipal.Eye.SRanipal_Eye_v2.GetGazeRay(ViveSR.anipal.Eye.GazeIndex.COMBINE, out ray))
+                {
+                    lastDir = GameplayReferences.HMD.TransformDirection(ray.direction);
+                }
+            }
+            return lastDir;
+        }
+#elif CVR_VARJO
+        static Vector3 lastDir = Vector3.forward;
+        static Vector3 GetLookDirection()
+        {
+            if (Varjo.VarjoPlugin.InitGaze())
+            {
+                var data = Varjo.VarjoPlugin.GetGaze();
+                if (data.status != Varjo.VarjoPlugin.GazeStatus.INVALID)
+                {
+                    var ray = data.gaze;
+                    lastDir = GameplayReferences.HMD.TransformDirection(new Vector3((float)ray.forward[0], (float)ray.forward[1], (float)ray.forward[2]));
+                }
+            }
+            return lastDir;
+        }
+#elif CVR_PICONEO2EYE
+        static Vector3 lastDirection = Vector3.forward;
+
+        static Vector3 GetLookDirection()
+        {
+            Pvr_UnitySDKAPI.EyeTrackingGazeRay gazeRay = new Pvr_UnitySDKAPI.EyeTrackingGazeRay();
+            var t = Pvr_UnitySDKManager.SDK.HeadPose.Matrix;
+            if (Pvr_UnitySDKAPI.System.UPvr_getEyeTrackingGazeRayWorld(ref gazeRay))
+            {
+                if (gazeRay.IsValid && gazeRay.Direction.sqrMagnitude > 0.1f)
+                {
+                    lastDirection = gazeRay.Direction;
+                }
+            }
+            return lastDirection;
+        }
+#elif CVR_OMNICEPT
+        static Vector3 lastDirection = Vector3.forward;
+        static HP.Omnicept.Unity.GliaBehaviour gb;
+
+        static void DoEyeTracking(HP.Omnicept.Messaging.Messages.EyeTracking data)
+        {
+            lastDirection = GameplayReferences.HMD.TransformDirection(new Vector3(data.CombinedGaze.X, data.CombinedGaze.Y, data.CombinedGaze.Z));
+        }
+
+        static Vector3 GetLookDirection()
+        {
+            if (gb == null)
+            {
+                gb = GameObject.FindObjectOfType<HP.Omnicept.Unity.GliaBehaviour>();
+                if (gb != null)
+                    gb.OnEyeTracking.AddListener(DoEyeTracking);
+            }
+            return lastDirection;
+        }
+#elif CVR_XR
+
+        static Vector3 lastDirection = Vector3.forward;
+        static Vector3 GetLookDirection()
+        {
+            UnityEngine.XR.Eyes eyes;
+            if (UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.CenterEye).TryGetFeatureValue(UnityEngine.XR.CommonUsages.eyesData, out eyes))
+            {
+                //first arg probably to mark which feature the value should return. type alone isn't enough to indicate the property
+                Vector3 convergancePoint;
+                if (eyes.TryGetFixationPoint(out convergancePoint))
+                {
+                    Vector3 leftPos = Vector3.zero;
+                    eyes.TryGetLeftEyePosition(out leftPos);
+                    Vector3 rightPos = Vector3.zero;
+                    eyes.TryGetRightEyePosition(out rightPos);
+
+                    Vector3 centerPos = (rightPos + leftPos) / 2f;
+
+                    var worldGazeDirection = (convergancePoint - centerPos).normalized;
+
+                    lastDirection = worldGazeDirection;
+                    return worldGazeDirection;
+                }
+            }
+            return lastDirection;
+        }
+#else
+        static Vector3 GetLookDirection()
+        {
+            return GameplayReferences.HMD.forward;
+        }
+#endif
+    }
+}
---
+++ Digicenter XR-1/Assets/CognitiveVR/Scripts/MicrophoneButton.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using CognitiveVR;
+using System.IO;
+#if CVR_AH
+using AdhawkApi;
+using AdhawkApi.Numerics.Filters;
+#endif
+
+//used in cognitivevr exit poll to call actions on the main exit poll panel
+
+namespace CognitiveVR
+{
+    [AddComponentMenu("Cognitive3D/Internal/Microphone Button")]
+    public class MicrophoneButton : VirtualButton
+    {
+        [Header("Gaze Settings")]
+        float _currentRecordTime;
+
+        [Header("Recording")]
+        public int RecordTime = 10;
+        private int outputRate = 16000;
+        AudioClip clip;
+        bool _recording;
+        bool _finishedRecording;
+
+        [Header("Visuals")]
+        public Image MicrophoneImage;
+        public Text TipText;
+
+        ExitPollSet questionSet;
+        public void SetExitPollQuestionSet(ExitPollSet questionSet)
+        {
+            this.questionSet = questionSet;
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (GameplayReferences.HMD == null) { return; }
+            FillAmount = 0;
+            UpdateFillAmount();
+        }
+
+
+        //if the player is looking at the button, updates the fill image and calls ActivateAction if filled
+        void Update()
+        {
+            if (GameplayReferences.HMD == null) { return; }
+            //if (ExitPoll.CurrentExitPollSet.CurrentExitPollPanel.NextResponseTimeValid == false) { return; }
+            if (_finishedRecording) { return; }
+            //if (ExitPoll.CurrentExitPollSet.CurrentExitPollPanel.IsClosing) { return; }
+
+            if (_recording)
+            {
+                _currentRecordTime -= Time.deltaTime;
+                UpdateFillAmount();
+                float volumeLevel = MicrophoneUtility.LevelMax(clip);
+                Vector3 newScale = new Vector3(0.8f, 0.1f + Mathf.Clamp(volumeLevel, 0, 0.7f), 0.8f);
+                MicrophoneImage.transform.localScale = Vector3.Lerp(MicrophoneImage.transform.localScale, newScale, 0.1f);
+
+                if (_currentRecordTime <= 0)
+                {
+                    Microphone.End(null);
+                    byte[] bytes;
+                    CognitiveVR.MicrophoneUtility.Save(clip, out bytes);
+                    string encodedWav = MicrophoneUtility.EncodeWav(bytes);
+                    questionSet.CurrentExitPollPanel.AnswerMicrophone(encodedWav); //TODO need some method of accessing question set on this component - probably pass in at start
+                    _finishedRecording = true;
+                }
+            }
+        }
+
+        protected override void LateUpdate()
+        {
+            //if (ExitPoll.CurrentExitPollSet.CurrentExitPollPanel.NextResponseTimeValid == false) { return; }
+            if (OnFill == null) { return; }
+            //if (ExitPoll.CurrentExitPollSet.CurrentExitPollPanel.IsClosing) { return; }
+
+            //set fill visual
+            //check for over fill threshold to activate action
+
+            if (focusThisFrame)
+            {
+                FillAmount += Time.deltaTime;
+                UpdateFillAmount();
+                if (FillAmount >= FillDuration)
+                {
+                    RecorderActivate();
+                }
+            }
+            else if (FillAmount > 0)
+            {
+                FillAmount = 0;
+                UpdateFillAmount();
+            }
+            focusThisFrame = false;
+        }
+
+        void RecorderActivate()
+        {
+            // Call this to start recording. 'null' in the first argument selects the default microphone. Add some mic checking later
+            clip = Microphone.Start(null, false, RecordTime, outputRate);
+            fillImage.color = Color.red;
+
+            GetComponentInParent<ExitPollPanel>().DisableTimeout();
+            _currentRecordTime = RecordTime;
+            _finishedRecording = false;
+            _recording = true;
+            TipText.text = "Recording...";
+        }
+
+        protected void UpdateFillAmount()
+        {
+            if (_recording)
+            {
+                fillImage.fillAmount = _currentRecordTime / RecordTime;
+            }
+            else
+            {
+                fillImage.fillAmount = FillAmount / FillDuration;
+            }
+        }
+    }
+}
