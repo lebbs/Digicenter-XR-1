f60d07f7bd3c22603bbfe6bcaa8b47d61160fb1f
lebbs <35425109+lebbs@users.noreply.github.com>
2020-12-29 14:34:10 +0200

    Backup

--- Digicenter XR-1/Assets/Scripts/SpawnCube.cs
+++ Digicenter XR-1/Assets/Scripts/SpawnCube.cs
@@ -11,12 +11,13 @@ public class SpawnCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnObject();
+      //  SpawnObject();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown("space"))
         {
             SpawnObject();
