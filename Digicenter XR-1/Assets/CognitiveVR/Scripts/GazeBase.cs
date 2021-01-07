40e061c39960d0927ed9ee1efa35145fb9638c61
lebbs <35425109+lebbs@users.noreply.github.com>
2021-01-07 15:05:37 +0200

    backup

--- Digicenter XR-1/Assets/CognitiveVR/Scripts/GazeBase.cs
+++ Digicenter XR-1/Assets/CognitiveVR/Scripts/GazeBase.cs
@@ -167,21 +167,7 @@ namespace CognitiveVR
                     didhitdynamic = true;
                     worldHitPoint = hit.point;
 
-                    Vector3 LocalGaze = hitDynamic.transform.InverseTransformPointUnscaled(worldHitPoint);
-                    float relativeScalex = hitDynamic.transform.lossyScale.x / hitDynamic.StartingScale.x;
-                    float relativeScaley = hitDynamic.transform.lossyScale.y / hitDynamic.StartingScale.y;
-                    float relativeScalez = hitDynamic.transform.lossyScale.z / hitDynamic.StartingScale.z;
-
-                    localHitPoint.x = LocalGaze.x / relativeScalex;
-                    localHitPoint.y = LocalGaze.y / relativeScaley;
-                    localHitPoint.z = LocalGaze.z / relativeScalez;
-
-                    //Vector3 relativeScale = new Vector3(
-                    //    hitDynamic.transform.lossyScale.x / hitDynamic.StartingScale.x,
-                    //    hitDynamic.transform.lossyScale.y / hitDynamic.StartingScale.y,
-                    //    hitDynamic.transform.lossyScale.z / hitDynamic.StartingScale.z);
-                    //localHitPoint = new Vector3(LocalGaze.x / relativeScale.x, LocalGaze.y / relativeScale.y, LocalGaze.z / relativeScale.z);
-
+                    localHitPoint = hitDynamic.transform.InverseTransformPoint(worldHitPoint);
                     hitDistance = hit.distance;
                     hitTextureCoord = hit.textureCoord;
                 }
@@ -202,20 +188,7 @@ namespace CognitiveVR
                     didhitdynamic = true;
                     worldHitPoint = hit.point;
 
-                    Vector3 LocalGaze = hitDynamic.transform.InverseTransformPointUnscaled(worldHitPoint);
-                    float relativeScalex = hitDynamic.transform.lossyScale.x / hitDynamic.StartingScale.x;
-                    float relativeScaley = hitDynamic.transform.lossyScale.y / hitDynamic.StartingScale.y;
-                    float relativeScalez = hitDynamic.transform.lossyScale.z / hitDynamic.StartingScale.z;
-
-                    localHitPoint.x = LocalGaze.x / relativeScalex;
-                    localHitPoint.y = LocalGaze.y / relativeScaley;
-                    localHitPoint.z = LocalGaze.z / relativeScalez;
-
-                    //Vector3 relativeScale = new Vector3(
-                    //    hitDynamic.transform.lossyScale.x / hitDynamic.StartingScale.x,
-                    //    hitDynamic.transform.lossyScale.y / hitDynamic.StartingScale.y,
-                    //    hitDynamic.transform.lossyScale.z / hitDynamic.StartingScale.z);
-                    //localHitPoint = new Vector3(LocalGaze.x / relativeScale.x, LocalGaze.y / relativeScale.y, LocalGaze.z / relativeScale.z);
+                    localHitPoint = hitDynamic.transform.InverseTransformPoint(worldHitPoint);
                     hitDistance = hit.distance;
                     hitTextureCoord = hit.textureCoord;
                 }
@@ -282,6 +255,7 @@ namespace CognitiveVR
             }    
 #elif CVR_VIVEPROEYE
             var ray = new Ray();
+            //improvement? - if using callback, listen and use last valid data instead of calling SRanipal_Eye_API.GetEyeData
             if (ViveSR.anipal.Eye.SRanipal_Eye.GetGazeRay(ViveSR.anipal.Eye.GazeIndex.COMBINE, out ray))
             {
                 gazeDirection = GameplayReferences.HMD.TransformDirection(ray.direction);
